7e5384b3ad70927cdbc1c03dff9ee00591bc575d
NormanBrandon <saldananormanbrandon1iv18@gmail.com>
2024-01-28 23:20:57 -0600

    Initial Commit

---
+++ HelloWord/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HelloWord
+//
+//  Created by Norman Brandon Saldaña Hernandez on 28/01/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!,\n I'm norman, a new developer for IOS \n and apple devices")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HelloWord/HelloWordApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWordApp.swift
+//  HelloWord
+//
+//  Created by Norman Brandon Saldaña Hernandez on 28/01/24.
+//
+
+import SwiftUI
+
+@main
+struct HelloWordApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
